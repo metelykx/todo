30ad776b8fdaa60c5a519397381d6c22a2dff7f7
metelykx <ivaschenkoodenys@gmail.com>
2025-09-13 12:18:26 +0300

    structs from JSON

--- todo/Model.swift
+++ todo/Model.swift
@@ -5,3 +5,23 @@
 //  Created by Denis Ivaschenko on 13.09.2025.
 //
 
+import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let todos: [Todo]
+    let total, skip, limit: Int
+}
+
+// MARK: - Todo
+struct Todo: Codable {
+    let id: Int
+    let todo: String
+    let completed: Bool
+    let userID: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id, todo, completed
+        case userID = "userId"
+    }
+}
